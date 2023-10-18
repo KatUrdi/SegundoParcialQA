459494711f711ed77e20254783ef9524edf74c70
Katzumi Urdininea <katurdininea02@gmail.com>
2023-10-18 16:31:47 -0400

    inicio

---
+++ src/test/java/clasesCRUDBasicAuth/Configuration.java
@@ -0,0 +1,13 @@
+package clasesCRUDBasicAuth;
+
+public class Configuration {
+
+    public static String user="katurdininea02@gmail.com";
+    public static String password="kat123";
+
+    public static String host="https://todo.ly";
+
+    public static String item_host="https://todo.ly/api/items";
+
+    public static String user_host="https://todo.ly/api/user";
+}
---
+++ src/test/java/selenium/testTodoly/UpdateNameTodolyTest.java
@@ -0,0 +1,51 @@
+package selenium.testTodoly;
+
+import org.junit.jupiter.api.*;
+import selenium.pagesTodoLy.LoginSection;
+import selenium.pagesTodoLy.MainPage;
+import selenium.pagesTodoLy.MenuSection;
+import selenium.pagesTodoLy.SettingsPopUp;
+import selenium.session.Session;
+
+import java.util.Random;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UpdateNameTodolyTest {
+
+    MenuSection menuSection = new MenuSection();
+    MainPage mainPage = new MainPage();
+    LoginSection loginSection = new LoginSection();
+
+    SettingsPopUp settingsPopUp = new SettingsPopUp();
+
+    static Random rand = new Random();
+    static String fullName;
+
+    @AfterEach
+    public void close(){
+        Session.getInstance().closeSession();
+    }
+
+    @BeforeEach
+    public void open() throws InterruptedException {
+        Session.getInstance().getBrowser().get("http://todo.ly/");
+        mainPage.loginButton.click();
+        loginSection.emailTextBox.setText("katurdininea02@gmail.com");
+        loginSection.passwordTextBox.setText("kat123");
+        loginSection.loginButton.click();
+        Thread.sleep(3000);
+    }
+
+    @Test
+    @Order(1)
+    public void updateFullName() throws InterruptedException{
+        fullName="KatzumiUrdininea"+rand.nextInt(500)+rand.nextInt(500);
+        menuSection.settings.click();
+        settingsPopUp.fullNameInput.clearTextField();
+        settingsPopUp.fullNameInput.setText(fullName);
+        settingsPopUp.okButton.click();
+        menuSection.settings.click();
+        String result = settingsPopUp.fullNameInput.getInput();
+        Assertions.assertEquals(fullName, result, "ERROR: no se pudo cambiar el nombre");
+    }
+}
